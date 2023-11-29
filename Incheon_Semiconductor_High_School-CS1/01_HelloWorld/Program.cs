52837f1a9b773205657d11665683d18b74d130bf
조하랑 <wawa111027@gmail.com>
2023-11-29 10:54:23 +0900

    C# 레벨 추가
    C# 레벨 추가

--- Incheon_Semiconductor_High_School-CS1/01_HelloWorld/Program.cs
+++ Incheon_Semiconductor_High_School-CS1/01_HelloWorld/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _001_HelloWorld
+namespace _01_HelloWorld
 {
     /* https://whgkfkd.tistory.com/77 */ // 출력 강의 사이트
     class Program
@@ -22,6 +21,18 @@ namespace _001_HelloWorld
             int y = 7;
 
             Console.WriteLine($"x: {x}, y: {y}");
+
+            float pi = 3.14159f; //실수형 선언 (뒤에 "f"는 필수)
+            Console.WriteLine($"원주율: {pi}");
+
+            string message = "Hello, World!";
+            Console.WriteLine(message); // message 안에 있는 Hello, World! 출력
+
+            string name = "홍길동";
+            int age = 25;
+
+            Console.WriteLine($"제 이름은 {name}이고 나이는 {age}살입니다.");
+            // 제 이름은 홍길동이고 나이는 25살입니다.
 
             Console.ReadKey();
         }
---
+++ Incheon_Semiconductor_High_School-CS1/02_HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_HelloWorld
+{
+    /* https://whgkfkd.tistory.com/77 */ // 출력 강의 사이트
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("다양한 출력");
+            Console.WriteLine("해보기");
+
+            Console.WriteLine("{0} * {1} = {2}", 2, 10, 200);
+
+            Console.WriteLine("    *    ");
+            Console.WriteLine("   ***   ");
+            Console.WriteLine("  *****  ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
